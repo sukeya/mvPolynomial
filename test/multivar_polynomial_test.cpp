f0899bb0c36afb49a391aaa825cb21a3088cde56
Yuya Asano <64895419+sukeya@users.noreply.github.com>
2023-06-16 16:39:27 +0900

    fix test data of MultiVarPolynomial.

--- test/multivar_polynomial_test.cpp
+++ test/multivar_polynomial_test.cpp
@@ -4,7 +4,6 @@
 #include "boost/test/unit_test.hpp"
 #include "multivar_polynomial/multivar_polynomial.hpp"
 
-#include <iostream>
 #include <vector>
 
 
@@ -17,11 +16,11 @@ BOOST_AUTO_TEST_CASE(multivar_polynomial_init, * utf::tolerance(tt::fpc::percent
   auto ans = std::vector<std::pair<Eigen::Vector2i, double>>();
   ans = {
     {{0, 0}, 1},
-    {{1, 0}, 1},
-    {{0, 1}, 1},
-    {{1, 1}, 1},
-    {{2, 0}, 1},
-    {{0, 2}, 1},
+    {{1, 0}, 2},
+    {{0, 1}, 3},
+    {{1, 1}, 4},
+    {{2, 0}, 5},
+    {{0, 2}, 6},
   };
 
   auto m = multivar_polynomial::MultiVarPolynomial<double, int, 2>(ans.begin(), ans.end());
@@ -37,16 +36,16 @@ BOOST_AUTO_TEST_CASE(multivar_polynomial_Of, * utf::tolerance(tt::fpc::percent_t
   auto ans = std::vector<std::pair<Eigen::Vector2i, double>>();
   ans = {
     {{0, 0}, 1},
-    {{1, 0}, 1},
-    {{0, 1}, 1},
-    {{1, 1}, 1},
-    {{2, 0}, 1},
-    {{0, 2}, 1},
+    {{1, 0}, 2},
+    {{0, 1}, 3},
+    {{1, 1}, 4},
+    {{2, 0}, 5},
+    {{0, 2}, 6},
   };
 
   auto m = multivar_polynomial::MultiVarPolynomial<double, int, 2>(ans.begin(), ans.end());
   BOOST_TEST(Of(m, Eigen::Vector2d::Zero()) == 1);
-  BOOST_TEST(Of(m, {2, 3}) == 25);
+  BOOST_TEST(Of(m, {2, 3}) == 112);
 }
 
 
@@ -55,17 +54,17 @@ BOOST_AUTO_TEST_CASE(multivar_polynomial_derivative, * utf::tolerance(tt::fpc::p
   auto ans = std::vector<std::pair<Eigen::Vector2i, double>>();
   ans = {
     {{0, 0}, 1},
-    {{1, 0}, 1},
-    {{0, 1}, 1},
-    {{1, 1}, 1},
-    {{2, 0}, 1},
-    {{0, 2}, 1},
+    {{1, 0}, 2},
+    {{0, 1}, 3},
+    {{1, 1}, 4},
+    {{2, 0}, 5},
+    {{0, 2}, 6},
   };
   auto m = multivar_polynomial::MultiVarPolynomial<double, int, 2>(ans.begin(), ans.end());
   ans = {
-    {{0, 0}, 1},
-    {{0, 1}, 1},
-    {{1, 0}, 2}
+    {{0, 0}, 2},
+    {{0, 1}, 4},
+    {{1, 0}, 10}
   };
   auto dm0 = multivar_polynomial::D(m, 0);
   for (auto i = 0; i < ans.size(); ++i)
@@ -73,9 +72,9 @@ BOOST_AUTO_TEST_CASE(multivar_polynomial_derivative, * utf::tolerance(tt::fpc::p
     BOOST_TEST(dm0[ans[i].first] == ans[i].second);
   }
   ans = {
-    {{0, 0}, 1},
-    {{1, 0}, 1},
-    {{0, 1}, 2}
+    {{0, 0}, 3},
+    {{1, 0}, 4},
+    {{0, 1}, 12}
   };
   auto dm1 = multivar_polynomial::D(m, 1);
   for (auto i = 0; i < ans.size(); ++i)
@@ -90,20 +89,20 @@ BOOST_AUTO_TEST_CASE(multivar_polynomial_integral, * utf::tolerance(tt::fpc::per
   auto ans = std::vector<std::pair<Eigen::Vector2i, double>>();
   ans = {
     {{0, 0}, 1},
-    {{1, 0}, 1},
-    {{0, 1}, 1},
-    {{1, 1}, 1},
-    {{2, 0}, 1},
-    {{0, 2}, 1},
+    {{1, 0}, 2},
+    {{0, 1}, 3},
+    {{1, 1}, 4},
+    {{2, 0}, 5},
+    {{0, 2}, 6},
   };
   auto m = multivar_polynomial::MultiVarPolynomial<double, int, 2>(ans.begin(), ans.end());
   ans = {
     {{0, 1}, 1},
-    {{1, 1}, 1},
-    {{0, 2}, 0.5},
-    {{1, 2}, 0.5},
-    {{2, 1}, 1},
-    {{0, 3}, 1.0 / 3}
+    {{1, 1}, 2},
+    {{0, 2}, 1.5},
+    {{1, 2}, 2},
+    {{2, 1}, 5},
+    {{0, 3}, 2}
   };
   auto sm = multivar_polynomial::Integrate(m, 1);
   for (auto i = 0; i < ans.size(); ++i)
