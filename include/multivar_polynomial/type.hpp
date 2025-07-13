63d9a75178c85d1ebdb460959fd073d0010f59bd
Yuya Asano <64895419+sukeya@users.noreply.github.com>
2025-07-13 22:16:17 +0900

    Restrict acceptable type of coord_type.

--- include/multivar_polynomial/type.hpp
+++ include/multivar_polynomial/type.hpp
@@ -9,7 +9,7 @@ namespace multivar_polynomial {
 template <std::signed_integral IntType, int D>
 using IndexType = Eigen::Array<IntType, D, 1>;
 
-template <class R, int D>
+template <std::floating_point R, int D>
 using CoordType = Eigen::Array<R, D, 1>;
 }  // namespace multivar_polynomial
 
