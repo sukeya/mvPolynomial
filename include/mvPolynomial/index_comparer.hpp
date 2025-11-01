3e34a479abb3449861ed4a8d4933d543b2becb0e
Yuya Asano <64895419+sukeya@users.noreply.github.com>
2025-11-01 20:21:43 +0900

    Reverse the order of comparing index.

--- include/mvPolynomial/index_comparer.hpp
+++ include/mvPolynomial/index_comparer.hpp
@@ -24,17 +24,17 @@ class IndexComparer final {
   static constexpr int dim = D;
 
   /**
-   * \brief If lhd[i] == rhd[i] for i = 0, ..., N - 1 and lhd[N] > rhd[N],
-   * return greater: if lhd[N] < rhd[N], return less: otherwise, return equal.
+   * \brief If lhd[i] == rhd[i] for i = 0, ..., N - 1 and lhd[N] < rhd[N],
+   * return greater: if lhd[N] > rhd[N], return less: otherwise, return equal.
    * \param[in] lhd an index
    * \param[in] rhd an index
    */
   static constexpr std::strong_ordering Compare(const Index& lhd, const Index& rhd) {
     for (std::size_t i = 0; i < lhd.size(); ++i) {
       auto comp = lhd[i] <=> rhd[i];
-      if (comp > 0) {
+      if (comp < 0) {
         return std::strong_ordering::greater;
-      } else if (comp < 0) {
+      } else if (comp > 0) {
         return std::strong_ordering::less;
       }
     }
@@ -42,16 +42,16 @@ class IndexComparer final {
   }
 
   /**
-   * \brief If lhd[i] == rhd[i] for i = 0, ..., N - 1 and lhd[N] > rhd[N],
+   * \brief If lhd[i] == rhd[i] for i = 0, ..., N - 1 and lhd[N] < rhd[N],
    * return true: otherwise, false.
    * \param[in] lhd an index
    * \param[in] rhd an index
    */
   bool operator()(const Index& lhd, const Index& rhd) const {
     for (std::size_t i = 0; i < lhd.size(); ++i) {
-      if (lhd[i] > rhd[i]) {
+      if (lhd[i] < rhd[i]) {
         return true;
-      } else if (lhd[i] < rhd[i]) {
+      } else if (lhd[i] > rhd[i]) {
         return false;
       }
     }
