f8adefbf431c2e2f5040c5ab734dda33a4605b3b
Yuya Asano <64895419+sukeya@users.noreply.github.com>
2025-11-29 23:09:55 +0900

    Small optimization for `*`.

--- include/mvPolynomial/mvPolynomial.hpp
+++ include/mvPolynomial/mvPolynomial.hpp
@@ -374,6 +374,15 @@ class MVPolynomial final {
   friend MVPolynomial operator*(const MVPolynomial& l, const MVPolynomial& r) {
     auto comparer = l.key_comp();
 
+    if (l.size() == 1) {
+      auto mul = r;
+      mul *= l;
+      return mul;
+    }
+    if (r.size() == 1) {
+      return r * l;
+    }
+
     auto mul = MVPolynomial(l.get_allocator());
     // Clear
     mul.erase(mul.begin());
