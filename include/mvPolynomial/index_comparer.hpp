392d8d7773e987ee2eb1d15a7fd57d94197350eb
Yuya Asano <64895419+sukeya@users.noreply.github.com>
2026-05-16 15:36:17 +0900

    Remove an unused member func.

--- include/mvPolynomial/index_comparer.hpp
+++ include/mvPolynomial/index_comparer.hpp
@@ -25,24 +25,6 @@ class IndexComparer final {
 
   /**
    * \brief If lhd[i] == rhd[i] for i = 0, ..., N - 1 and lhd[N] < rhd[N],
-   * return greater: if lhd[N] > rhd[N], return less: otherwise, return equal.
-   * \param[in] lhd an index
-   * \param[in] rhd an index
-   */
-  static constexpr std::strong_ordering Compare(const Index& lhd, const Index& rhd) {
-    for (int i = 0; i < lhd.size(); ++i) {
-      auto comp = lhd[i] <=> rhd[i];
-      if (comp < 0) {
-        return std::strong_ordering::greater;
-      } else if (comp > 0) {
-        return std::strong_ordering::less;
-      }
-    }
-    return std::strong_ordering::equal;
-  }
-
-  /**
-   * \brief If lhd[i] == rhd[i] for i = 0, ..., N - 1 and lhd[N] < rhd[N],
    * return true: otherwise, false.
    * \param[in] lhd an index
    * \param[in] rhd an index
